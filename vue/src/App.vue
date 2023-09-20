4742c314201b408ccf79ff700afac50fb24fd14b
charlieop <spchanae@connect.ust.hk>
2023-09-20 16:07:39 +0800

    removed unused app.vue style

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,59 +7,7 @@
   </container>
 </template>
 <script setup>
-import HeaderBar from './components/HeaderBar.vue'
+import HeaderBar from "./components/general/NavBar.vue";
 </script>
 
-<style>
-
-html,body{
-  width: 100%;
-  height: 100%;
-  margin: 0;
-}
-#app{
-  height: 100vh;
-  width: 100%;
-}
-.el-header {
-  background-color: #1961B1;
-  color: #fff;
-  line-height: 60px;
-  --el-header-padding: 0 10px;
-  width: 100%;
-}
-
-.el-main {
-  --el-main-padding: 0;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-</style>
+<style></style>
--- vue/src/components/general/NavBar.vue
+++ vue/src/components/general/NavBar.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="content">
-    <img src="../assets/imgs/icons/logo.svg" alt="contain" />
+    <img src="../../assets/imgs/icons/logo.svg" alt="contain" />
     <div class="content-line"></div>
     <div class="content-nav">
       <div class="content-nav-words">
-        <div :class="'content-nav-words-item '+active[0]" @click="change(0)">
+        <div :class="'content-nav-words-item ' + active[0]" @click="change(0)">
           <div class="content-nav-words-item-number">00</div>
           <div class="content-nav-words-item-word">home</div>
         </div>
-        <div :class="'content-nav-words-item '+active[1]" @click="change(1)">
-          <div class="content-nav-words-item-number ">01</div>
-          <div class="content-nav-words-item-word">DESTINATION</div>
+        <div :class="'content-nav-words-item ' + active[1]" @click="change(1)">
+          <div class="content-nav-words-item-number">01</div>
+          <div class="content-nav-words-item-word">destination</div>
         </div>
-        <div :class="'content-nav-words-item '+active[2]" @click="change(2)">
+        <div :class="'content-nav-words-item ' + active[2]" @click="change(2)">
           <div class="content-nav-words-item-number">02</div>
-          <div class="content-nav-words-item-word">CREW</div>
+          <div class="content-nav-words-item-word">crew</div>
         </div>
-        <div :class="'content-nav-words-item '+active[3]" @click="change(3)">
+        <div :class="'content-nav-words-item ' + active[3]" @click="change(3)">
           <div class="content-nav-words-item-number">03</div>
-          <div class="content-nav-words-item-word">TECHNOLOGY</div>
+          <div class="content-nav-words-item-word">technology</div>
         </div>
       </div>
     </div>
@@ -30,13 +30,12 @@ var active = ref(["", "", "", "active"]);
 function change(index) {
   for (var i = 0; i < active.value.length; i++) {
     if (i != index) {
-        active.value[i] = "";
+      active.value[i] = "";
     }
   }
   active.value[index] = "active";
   console.log(active.value);
 }
-
 </script>
 <style scoped>
 .content {
@@ -83,8 +82,8 @@ function change(index) {
   margin-right: 48px;
   cursor: pointer;
 }
-.content-nav-words-item:hover{
-    border-bottom: 3px rgba(255, 255, 255, 0.5) solid ;
+.content-nav-words-item:hover {
+  border-bottom: 3px rgba(255, 255, 255, 0.5) solid;
 }
 .content-nav-words-item-number {
   margin-right: 13px;
@@ -103,8 +102,8 @@ function change(index) {
   line-height: normal;
   letter-spacing: 2.7px;
 }
-.active{
-border-bottom: 3px #FFFFFF solid;
-pointer-events: none;
+.active {
+  border-bottom: 3px #ffffff solid;
+  pointer-events: none;
 }
 </style>
