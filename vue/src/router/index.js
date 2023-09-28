b9bfc461c938fd6e3a564de6259e0f200bea08a7
charlieop <spchanae@connect.ust.hk>
2023-09-28 20:08:00 +0800

    added DestView

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DestView from '../views/DestView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: DestView
   }
 ]
 
---
+++ vue/src/views/DestView.vue
@@ -0,0 +1,7 @@
+<template>
+  <Dest__Main />
+</template>
+<script setup>
+import Dest__Main from '@/components/dest/Dest__Main.vue';
+
+</script>
